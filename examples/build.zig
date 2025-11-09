7204a3b7655dd856174a05c7f4e1f17eb27f8161
Bilal <bjlegend007@gmail.com>
2025-11-09 16:05:44 +0530

    fixed the dynamic library problem in examples file

--- examples/build.zig
+++ examples/build.zig
@@ -30,17 +30,23 @@ pub fn build(b: *std.Build) void {
     const glfw_package = b.dependency("glfw", .{
         .target = target,
         .optimize = optimize,
-        .link_type = .dynamic
+        .link_type = .static
     });
-    opengl_exe.linkLibrary(glfw_package.artifact("glfw"));
-    opengl_exe.root_module.addImport("glfw", glfw_package.module("glfw"));
-    const glfw_artifact = glfw_package.artifact("glfw");
-    const install_glfw_artifact = b.addInstallArtifact(glfw_artifact,
-        .{.dest_dir = .{.override = .bin}}
-    );
     
+    opengl_exe.root_module.addImport("glfw", glfw_package.module("glfw"));
+
     const install_opengl_example = b.addInstallArtifact(opengl_exe, .{});
     const example_step = b.step("examples", "build all the example files");
     example_step.dependOn(&install_opengl_example.step);
-    example_step.dependOn(&install_glfw_artifact.step);
+
+    if(glfw_package.builder.user_input_options.get("link_type")) |link_type|{
+    if(std.mem.eql(u8, link_type.value.scalar, "dynamic")){
+            opengl_exe.linkLibrary(glfw_package.artifact("glfw"));
+            const glfw_artifact = glfw_package.artifact("glfw");
+            const install_glfw_artifact = b.addInstallArtifact(glfw_artifact,
+                .{.dest_dir = .{.override = .bin}}
+            );
+            example_step.dependOn(&install_glfw_artifact.step);
+        }
+    }
 }
